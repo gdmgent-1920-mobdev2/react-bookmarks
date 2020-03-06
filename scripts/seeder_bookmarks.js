e1f36371b8819f6bfcf97020a4d6749cd1c7f9f1
unknown <Administrator@DMARB0201C687WX.student.arteveldehs.be>
2020-03-06 09:22:01 +0100

    Added proxy and bookmark type in seeder_bookmarks

--- scripts/seeder_bookmarks.js
+++ scripts/seeder_bookmarks.js
@@ -1,16 +1,51 @@
+import fetch from 'node-fetch';
+
 import { admin, app, db } from './firebase';
 
-const createBookmark = async (title, description) => {
-    const bookmark = {
-        title,
-        description,
-        createdAt: Date.now()
-    };
+const urlsToBookmark = [
+    'https://github.com/gdmgent-1920-mobdev2/nodejs-nmd-crawler',
+    'https://devcenter.heroku.com/articles/getting-started-with-nodejs',
+    'https://www.npmjs.com/package/ts-node',
+    'https://vuejs.org/',
+    'https://svelte.dev',
+    'https://angular.io',
+    'https://emberjs.com',
+    'https://reactjs.org',
+    'https://www.webcomponents.org',
+    'https://webassembly.org/',
+    'https://www.meteor.com',
+    'https://hapi.dev',
+    'https://nestjs.com',
+];
+
+const PROXY_NMD_CRAWLER_SEO_URL = 'https://nodejs-nmd-crawler.herokuapp.com/proxy/seo?url=';
+
+const getSEOFromUrl = async (url) => {
+    const response = await fetch(`${PROXY_NMD_CRAWLER_SEO_URL}${url}`);
+    const json = await response.json();
+    return json;
+};  
+
+const createBookmark = async (bookmark) => {
     await db.collection('bookmarks').add(bookmark);
-}
+};
 
 (async () => {
-    for (let i = 0; i < 100; i++) {
-        await createBookmark(`Title ${i}`, `Description ${i*i}`);
-    }
+    urlsToBookmark.forEach(async (url, index) => {
+        const metadata = await getSEOFromUrl(url);
+        const bookmark = {
+            url: metadata.url || null,
+            title:  metadata.title || null,
+            description:  metadata.description || null,
+            provider:  metadata.provider || null,
+            icon:  metadata.icon || null,
+            image:  metadata.image || null,
+            language:  metadata.language || null,
+            keywords:  metadata.keywords || null,
+            type:  metadata.type || null,
+            createdAt: Date.now(),
+            modifiedAt: null
+        };
+        await createBookmark(bookmark);
+    });
 })()
